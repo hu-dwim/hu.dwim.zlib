125de7fbfe7e2e7a254a07a1c0469dbff26cbfc4
Attila Lendvai <attila@lendvai.name>
2024-02-27 23:02:58 +0100

    housekeeping: add notes to bin/filter-spec-files.sh

--- bin/filter-spec-files.sh
+++ bin/filter-spec-files.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# This file is called by generate-spec-files.sh. No need to run it manually.
+
 # https://stedolan.github.io/jq/manual/
 
 patterns=(
@@ -19,8 +21,19 @@ for i in "${!patterns[@]}"; do
     for file in ${patterns[$i]}; do
         if [ -e ${file} ]; then # avoid creating a 'zlib.*.spec' if it didn't match anything
             echo "Running jq --sort-keys '${queries[$i]}' ${file}"
-            jq --sort-keys "${queries[$i]}" ${file} >${file}.filtered
-            mv ${file}.filtered ${file}
+            jq --sort-keys "${queries[$i]}" ${file} >${file}.filtered &&
+                mv ${file}.filtered ${file}
+            # This removes the location entries which shrinks
+            # the diff size.
+            # NOTE: this cannot be done, because cffi/c2ffi uses
+            # the location field to decide whether to include the
+            # definition or not.
+            # Remove from the toplevel entries:
+            # jq 'del( .[] .location )' ${file} >${file}.filtered &&
+            #     mv ${file}.filtered ${file}
+            # Remove from nested entries, too:
+            # jq 'map(del(.location))' ${file} >${file}.filtered &&
+            #     mv ${file}.filtered ${file}
         fi
     done
 done
